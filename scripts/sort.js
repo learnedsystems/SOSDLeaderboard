2de45898febc72d71579097c61f79b099fb08a99
Allen Huang <huanga@mit.edu>
2021-03-08 20:49:46 -0500

    Made triangles bigger

--- scripts/sort.js
+++ scripts/sort.js
@@ -7,7 +7,7 @@ $(document).ready( function() {
         $(this).click();
         sortfwdind = document.createElement('span');
         sortfwdind.id = "sorttable_sortfwdind";
-        sortfwdind.innerHTML = stIsIE ? '&nbsp<font face="webdings">6</font>' : '&nbsp;&#x25BE;';
+        sortfwdind.innerHTML = stIsIE ? '&nbsp<font face="webdings">6</font>' : '&nbsp;&#x25BC;';
         $(this).appendChild(sortfwdind);
     })
 })
