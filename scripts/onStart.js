7c478a97a763dadd946197ec5c7090e610999b78
Allen <huanga@mit.edu>
2021-07-28 01:20:12 -0400

    Revert all changes

--- scripts/onStart.js
+++ scripts/onStart.js
@@ -26,13 +26,6 @@ starts_plotted = [
     "RS"
 ]
 
-learned_indexes = [
-    "RMI",
-    "RS",
-    "ALEX",
-    "PGM"
-]
-
 for (const indexName of starts_checked) {
     document.getElementById(indexName).checked = true;
 }
@@ -42,21 +35,13 @@ $(document).ready(function() {
         $(this).find('tbody > tr').each (function() {
             var trow = $(this);
             if (trow.index() >= 0) {
-                const nameCell = trow.find("td:first")
-                const indexValue = nameCell.text().trim();
+                var indexValue = trow.find("td:first").text().trim();
                 if (starts_plotted.includes(indexValue)) {
                     trow.prepend($(`<td>Plot: <button class="button_${indexValue}" onclick="toggleIndex('${indexValue}')">Remove</button>`));
                 } else {
                     trow.prepend($(`<td>Plot: <button class="button_${indexValue}" onclick="toggleIndex('${indexValue}')">Add</button>`));
                 }
-            }
-            const td = trow.find('td:nth-child(2)');
-            if (learned_indexes.includes(td.text.trim())) {
-                console.log(indexValue)
-                td.css("background-color", "#81fca4");
-            } else {
-                td.css("background-color", "#89ebfa");
-            }
+            } 
         });
     });
     jQuery('input[type="checkbox"][name="filter"]').on('change', filter_magic);
